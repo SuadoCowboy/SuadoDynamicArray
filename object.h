a18ba33b59ef57fb43c90d71d198ccd90fad4c19
SuadoCowboy <luccacsg@gmail.com>
2025-04-05 22:31:48 -0300

    add missing parameter(it is empty cuz we not using declspec or anything)

--- object.h
+++ object.h
@@ -6,5 +6,5 @@ typedef struct Object {
 	unsigned short value;
 } Object;
 
-SDA_DEFINE_ARRAY_HEADER(Object, obj_array, objarr_, unsigned short)
+SDA_DEFINE_ARRAY_HEADER(Object, obj_array, objarr_, unsigned short,)
 void objarr_clear(obj_array* pArray);
